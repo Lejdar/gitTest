44bde3db9300bfa5f8ce6ede8fed805bd306cf31
Lejdar <72464582>
2022-03-28 21:03:36 +0200

    Test gitu

--- src/main.java
+++ src/main.java
@@ -2,7 +2,7 @@ public class main {
 
     public static void main(String args[])  //static method
     {
-        System.out.println("Static method");
+        System.out.println("Static metsshod");
     }
 
 }
